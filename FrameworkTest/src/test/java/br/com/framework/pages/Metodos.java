f5d875e49208a444c80b27cb40230bc19a45257d
Luis Felipe <luisfelipe_9831@hotmail.com>
2020-10-28 21:44:27 -0300

    MetodoNavegadorIncluso

--- FrameworkTest/src/test/java/br/com/framework/pages/Metodos.java
+++ FrameworkTest/src/test/java/br/com/framework/pages/Metodos.java
@@ -1,11 +1,31 @@
 package br.com.framework.pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Metodos {
    WebDriver driver; 
    
-   public void abrirNavegador (String url, String navegador) {
-	   
-   }
+   public void abrirNavegador(String site, String navegador) {
+
+		if (navegador == "CHROME" || navegador == "FIREFOX") {
+			if (navegador == "CHROME") {
+				System.setProperty("webdriver.chrome.driver", "./Drivers/chromedriver.exe");
+				driver = new ChromeDriver();
+				driver.get(site);
+			} else if (navegador == "FIREFOX") {
+				if (navegador == "FIREFOX") {
+					System.setProperty("webdriver.gecko.driver", "./Drivers/geckodriver.exe");
+					driver = new FirefoxDriver();
+					driver.get(site);
+				}
+
+			}
+
+		} else {
+			System.out.println("Navegador inválido, digite CHROME ou FIREFOX");
+		}
+		driver.manage().window().maximize();
+	}
 }
